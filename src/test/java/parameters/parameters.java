e87e7d3f653f06ccedb6973a8d3e170a44550b77
Alexey Yakovtsov <alexey.yakovtsov@zensoft.io>
2019-09-03 13:22:45 +0300

    Add new files

---
+++ src/test/java/parameters/parameters.java
@@ -0,0 +1,14 @@
+package parameters;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+
+public class parameters {
+
+
+    public static String
+        URL_Dev = "https://dev.zenio.co",
+        URL_Dev_API = "https://dev.zenio.co/api",
+        URL_Dev_Login = "https://dev.zenio.co/login";
+}
